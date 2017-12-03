7723ed8decfe0ef221dd82c9b327e2202ede8c7a
TobyGE <15188313009@163.com>
2017-12-03 16:31:08 -0500

    final

--- LandMark.py
+++ LandMark.py
@@ -5,12 +5,13 @@ import random
 import copy
 import numpy as np
 import math
+import scipy.cluster.vq as vq
 
 class LandMark:
     def __init__(self):
         self.matrix = np.eye(2)
 
-    def getmatrix(self, matrix, k, density):
+    def getmatrix(self, matrix, k, r, density):
         self.matrix = matrix
         length_m = len(matrix)
         indexes = random.sample(range(length_m), (int)(length_m * density))
@@ -24,8 +25,13 @@ class LandMark:
             for j in range(length_u):
                 Z[i,j] = self.gaussian(self.matrix[i],U[j])
 
-        temp1 = Z.sum(axis=1)
-
+        #temp1 = Z.sum(axis=1)
+        temp1 = np.zeros(length_m)
+        for i in range(length_m):
+            arr = copy.copy(Z[i])
+            arr.sort()
+            for j in range(r):
+                temp1[i] += arr[j]
         for i in range(length_m):
             Z[i] = Z[i] / temp1[i]
 
@@ -34,8 +40,17 @@ class LandMark:
         for i in range(length_u):
             D[i,i] = 1 / math.sqrt(temp2[i])
         Z_head = np.dot(Z, D)
+        A, S, B = np.linalg.svd(np.transpose(Z_head),full_matrices=False)
+        B = np.transpose(B)
 
+        means, lables = vq.kmeans2(B[:,1:k],k)
+        print(lables)
+        #print(B.shape)
 
+        # print(Z_head.shape)
+        # print(A.shape)
+        # print(S.shape)
+        # print(B.shape)
         #print(Z)
         #temp = Z.sum(axis=1)
         #print(temp)
@@ -43,7 +58,7 @@ class LandMark:
         #print(self.matrix)
         #print(U)
         #print(Z)
-        print(np.dot(Z, U))
+        #print(np.dot(Z, U))
         #W = np.dot(Z, Z.T)
         #print(W)
         #temp = W.sum(axis=1)
@@ -55,5 +70,5 @@ class LandMark:
 
 if __name__ == "__main__":
     lm = LandMark()
-    map = np.array([[1,2,3],[4,5,6],[7,8,9],[10,11,12],[13,14,15]])
-    lm.getmatrix(map, 1, 0.7)
+    map = np.array([[1,2,3],[4,5,6],[7,8,9],[10,11,12],[13,14,15],[16,17,18],[19,20,21],[22,23,24]])
+    lm.getmatrix(map, 3, 3, 0.7)
